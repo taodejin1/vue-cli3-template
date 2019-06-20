4a7d41c0646ca9eceaf961260b4fd9148d0cdbed
664059235 <664059235@qq.com>
2019-06-20 17:50:25 +0800

    add vconsole tools

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import moment from 'moment' // 加载moment时间日期处理插件
 Vue.prototype.$moment = moment;
 
 import vconsole from 'vconsole' // 移动端console调试日志
-// new vconsole();
+new vconsole();
 
 import api from './server-api/api.js'   // 请求后端的服务器接口url地址
 import flexible from './assets/js/flexible.js' // 引入rem换算px脚本  640设计稿 1rem=100px
